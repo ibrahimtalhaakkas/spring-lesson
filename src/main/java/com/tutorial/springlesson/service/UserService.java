3aa385e541d4f0611ef2aaf236bfcd0631fdc519
ibrahimtalhaakkas <ibrahimtalhaakkas@gmail.com>
2021-08-12 21:30:20 +0200

    service layer is created

---
+++ src/main/java/com/tutorial/springlesson/service/UserService.java
@@ -0,0 +1,16 @@
+package com.tutorial.springlesson.service;
+
+
+import org.springframework.stereotype.Service;
+
+@Service // it contains @Component to be scanned
+public class UserService {
+
+    public String getMainPageContent() {
+        return "Welcome!";
+    }
+
+    public String getSayHelloContent() {
+        return "Say Hello :)";
+    }
+}
